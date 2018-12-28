2a7cf34c2c495afac4497dced9f981285610e585
Sasha Patsel <user@users-mbp.attlocal.net>
2018-12-27 16:14:35 -0800

    add boards view and serve individual boards to it

--- myproject/boards/views.py
+++ myproject/boards/views.py
@@ -4,8 +4,11 @@ from django.http import HttpResponse
 from .models import Board
 
 # Create your views here.
-def home(request, username):
-    print(username)
+def home(request):
     boards = Board.objects.all()
     return render(request, 'home.html', {'boards': boards})
+
+def board_topics(request, pk):
+    board = Board.objects.get(pk=pk)
+    return render(request, 'topics.html', {'board': board})
 
--- myproject/myproject/urls.py
+++ myproject/myproject/urls.py
@@ -19,7 +19,7 @@ from django.urls import path
 from boards import views
 
 urlpatterns = [
-    path("index/<slug:username>/", views.home, name="home"),
-    # path("boards/",views.board_topics, name="board_topics"),
+    path("", views.home, name="home"),
+    path("boards/<int:pk>/",views.board_topics, name="board_topics"),
     path('admin/', admin.site.urls),
 ]
