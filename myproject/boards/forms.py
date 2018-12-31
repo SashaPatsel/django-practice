9caf8d3b91c812f0d16a7d5545d9db17f3b0dc14
Sasha Patsel <user@users-mbp.attlocal.net>
2018-12-31 11:51:53 -0800

    render bootstrap form using django and bootstrap in new_topic

--- myproject/boards/forms.py
+++ myproject/boards/forms.py
@@ -3,7 +3,11 @@ from .models import Topic
 
 # This is a ModelForm associated with the Topic model. The subject in the fields list inside the Meta class is referring to the subject field in the Topic class.
 class NewTopicForm(forms.ModelForm):
-    message = forms.CharField(widget=forms.Textarea(), max_length=4000)
+    message = forms.CharField(
+        widget=forms.Textarea(),
+        max_length=4000,
+        help_text='The max length of the text is 4000.'
+    )
 
     class Meta:
         # Communicate that this form is associated with the Topic model
