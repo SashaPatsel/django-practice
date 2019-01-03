c00f5c15eb5b485d0205c2e492c1a70d36652842
Sasha Patsel <user@users-mbp.attlocal.net>
2019-01-03 13:46:59 -0800

    password reset paths and template files

--- myproject/myproject/urls.py
+++ myproject/myproject/urls.py
@@ -15,6 +15,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.conf.urls import url
 from django.contrib.auth import views as auth_views
 
 from accounts import views as accounts_views
@@ -28,4 +29,22 @@ urlpatterns = [
     path("boards/<int:pk>/",views.board_topics, name="board_topics"),
     path("boards/<int:pk>/new", views.new_topic, name='new_topic'),
     path('admin/', admin.site.urls),
+    
+    path("reset/",
+    auth_views.PasswordResetView.as_view(
+        template_name='password_reset.html',
+        email_template_name='password_reset_email.html',
+        subject_template_name='password_reset_subject.txt'
+    ),
+    name='password_reset'),
+    path("reset/done/",
+        auth_views.PasswordResetDoneView.as_view(template_name='password_reset_done.html'),
+        name='password_reset_done'),
+    #Use regex to check for valid email 
+    url(r'^reset/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/$',
+    auth_views.PasswordResetConfirmView.as_view(template_name='password_reset_confirm.html'),
+    name='password_reset_confirm'),
+    path("reset/complete/",
+        auth_views.PasswordResetCompleteView.as_view(template_name='password_reset_complete.html'),
+        name='password_reset_complete'),
 ]
