b5eee3a8bfeea22ef818abc22afc3ca90dc316d5
Sasha Patsel <user@users-mbp.attlocal.net>
2019-01-02 18:03:09 -0800

    modularize account tests w/ new folder

--- myproject/accounts/tests/test_view_signup.py
+++ myproject/accounts/tests/test_view_signup.py
@@ -3,7 +3,9 @@ from django.contrib.auth.forms import UserCreationForm
 from django.urls import reverse
 from django.urls import resolve
 from django.test import TestCase
-from .views import signup
+
+from ..views import signup
+from ..forms import SignUpForm
 
 class SignUpTests(TestCase):
     def setUp(self):
@@ -19,21 +21,33 @@ class SignUpTests(TestCase):
 
     def test_csrf(self):
         self.assertContains(self.response, 'csrfmiddlewaretoken')
-
+    
     def test_contains_form(self):
         form = self.response.context.get('form')
-        self.assertIsInstance(form, UserCreationForm)
+        self.assertIsInstance(form, SignUpForm)
+    
+    def test_form_inputs(self):
+        '''
+        The view must contain five inputs: csrf, username, email,
+        password1, password2
+        '''
+        self.assertContains(self.response, '<input', 5)
+        self.assertContains(self.response, 'type="text"', 1)
+        self.assertContains(self.response, 'type="email"', 1)
+        self.assertContains(self.response, 'type="password"', 2)
 
 class SuccessfulSignUpTests(TestCase):
     def setUp(self):
         url = reverse('signup')
         data = {
             'username': 'john',
+            'email': 'john@doe.com',
             'password1': 'abcdef123456',
             'password2': 'abcdef123456'
         }
         self.response = self.client.post(url, data)
         self.home_url = reverse('home')
+
 
     def test_redirection(self):
         '''
