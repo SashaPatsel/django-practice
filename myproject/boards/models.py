d660088288fd9db6e13209194b9aa9feaaf64ae9
Sasha Patsel <user@users-mbp.attlocal.net>
2019-01-12 16:42:41 -0800

    finish section 5: django orm

--- myproject/boards/models.py
+++ myproject/boards/models.py
@@ -17,12 +17,18 @@ class Board(models.Model):
     def get_last_post(self):
         return Post.objects.filter(topic__board=self).order_by('-created_at').first()
 
+    def board_topics(request, pk):
+        board = get_object_or_404(Board, pk=pk)
+        topics = board.topics.order_by('-last_updated').annotate(replies=Count('posts') - 1)
+        return render(request, 'topics.html', {'board': board, 'topics': topics})
+
 
 class Topic(models.Model):
     subject = models.CharField(max_length=255)
     last_updated = models.DateTimeField(auto_now_add=True)
     board = models.ForeignKey(Board, related_name='topics', on_delete=models.CASCADE)
     starter = models.ForeignKey(User, related_name='topics', on_delete=models.DO_NOTHING)
+    views = models.PositiveIntegerField(default=0)
 
     def __str__(self):
         return self.subject
@@ -40,3 +46,4 @@ class Post(models.Model):
     def __str__(self):
         truncated_message = Truncator(self.message)
         return truncated_message.chars(30)
+
